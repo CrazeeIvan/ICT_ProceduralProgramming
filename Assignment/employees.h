652eadbe4cf497ca8b2bfcf8320935aa916d5cae
CrazeeIvan <ciaranmaher@gmail.com>
2016-04-26 01:37:34 +0100

    Refactoring to Struct

--- Assignment/employees.h
+++ Assignment/employees.h
@@ -1,12 +1,18 @@
 #include <stdio.h>
+int i =0;
 
-int Id, payRate, hours, gross;
-char firstName[50];
-char secondName[50];
-char department[5];
-char path[] = "C:\\Users\\Ciaran\\Documents\\ICT_ProceduralProgramming\\Assignment\\employee.txt";
+char path[] = "employee.txt";
 void printDetails();
+void getDetails();
 
+struct e
+{
+  int Id, payRate, hours, gross;
+  char firstName[50];
+  char secondName[50];
+  char department[5];
+};
+struct e emp[4];
 
 void employeeMenu()
 {
@@ -23,53 +29,96 @@ void addEmployee()
   char confirm;
   printf("\n\t\t##Add Employee##\n");
 
-  printf("Please enter employee first name:\n");
-  scanf("%s", &firstName);
-  printf("Please enter employee first name:\n");
-  scanf("%s", &secondName);
-  printf("Please enter employee department:\n");
-  scanf("%s", &department);
-  printf("Please enter employee pay rate (euro per hour):\n");
-  scanf("%d", &payRate);
+  getDetails();
+
+
+
   printDetails();
   printf("\n\n\n\t\tSave?\n\t\t\t1. Yes/2. No\n");
-  scanf("%1d", &confirm);
-  if (confirm == 1)
+  while (confirm != 1 || confirm != 2)
   {
-    // Save file
-    writeDetails();
-    printf("\n\nThe submitted details were saved to %s\n\n", path);
+    scanf("%1d", &confirm);
+    if (confirm == 1)
+    {
+      // Save file
+      writeDetails();
+      printf("\n\nThe submitted details were saved to %s\n\n", path);
+    }
+    else if (confirm == 2)
+    {
+      // Prompt 'go back?' then go back to employee menu
+    }
+    else if (confirm == 3)
+    {
+      // Invalid input, try again.
+      printf("\n\nInvalid input, please try again.\n");
+    }
   }
-  else if (confirm == 2)
-  {
-    // Prompt 'go back?' then go back to employee menu
-  }
-  else
-  {
-    // Invalid input, try again.
-  }
+
 }
 
 
 int writeDetails()
 {
-  FILE *fptr;
-  fptr=(fopen(path,"a"));
-  if(fptr==NULL){
-      printf("Error!\n\t\tError 1101: File write error!");
-      return 1;
-  }
-  hours = 1;
-  gross = payRate * hours;
-  Id = 1001;
-  fprintf(fptr,"\nName=%s %s \nDepartment=%s \nID=%d \nPay Rate=%d \nhours=%d \ngross=%d\n\n", firstName, secondName, department, Id, payRate, hours, gross);
-  fclose(fptr);
-  return 0;
+  // FILE *fptr;
+  // fptr=(fopen(path,"a"));
+  // if(fptr==NULL){
+  //     printf("Error!\n\t\tError 1101: File write error!");
+  //     return 1;
+  // }
+  // fprintf(fptr,"\nName=%s %s \nDepartment=%s \nID=%d \nPay Rate=%d \nhours=%d \ngross=%d\n\n", emp.firstName, emp.secondName, emp.department, emp.Id, emp.payRate, emp.hours, emp.gross);
+  // fclose(fptr);
+  // return 0;
 }
 
-void printDetails()
+
+void printDetails(int i)
 {
-  printf("\n\t\tEmployee name:\t\t %s %s", firstName, secondName);
-  printf("\n\t\tDepartment:\t\t %s", department);
-  printf("\n\t\tPay rate:\t\t e%dp/h", payRate);
+  printf("\n\t\tEmployee name:\t\t %s %s", emp[i].firstName, emp[i].secondName);
+  printf("\n\t\tDepartment:\t\t %s", emp[i].department);
+  printf("\n\t\tPay rate:\t\t e%dp/h", emp[i].payRate);
+  printf("\n\t\tPay rate:\t\t %d", emp[i].hours);
+  printf("\n\t\tPay rate:\t\t %d", emp[i].gross);
+  printf("\n\t\tPay rate:\t\t %d", emp[i].Id);
+}
+
+void getDetails()
+{
+
+  FILE *fptr;
+  fptr=fopen(path,"wb");
+  for(i=0;i<5;++i)
+  {
+    fflush(stdin);
+    printf("Please enter employee first name:\n");
+    gets(emp[i].firstName);
+    printf("Please enter employee first name:\n");
+    gets(emp[i].secondName);
+    printf("Please enter employee department:\n");
+    gets(emp[i].department);
+    printf("%d", i);
+    printf("Please enter employee pay rate (euro per hour):\n");
+    scanf("%d",&emp[i].payRate);
+    emp[i].hours = 0;
+    emp[i].gross = emp[i].payRate * emp[i].hours;
+    emp[i].Id = 1001;
+  }
+  if(fptr==NULL)
+  {
+      printf("Error!\n\t\tError 1101: File write error!");
+  }
+  // fprintf(fptr,"\nName=%s %s \nDepartment=%s \nID=%d \nPay Rate=%d \nhours=%d \ngross=%d\n\n", emp.firstName, emp.secondName, emp.department, emp.Id, emp.payRate, emp.hours, emp.gross);
+  // fclose(fptr);
+
+
+  fwrite(emp, sizeof(emp), 1, fptr);
+  fclose(fptr);
+  // fptr=fopen("file.txt","rb");
+  // fread(b,sizeof(b),1,fptr);
+  // for(i=0;i<5;++i)
+  // {
+  //     printf("Name: %s\nHeight: %d",b[i].name,b[i].height);
+  // }
+  // fclose(fptr);
+  // i++;
 }
